a0d60104d0e35543cd349e4de4be116b5dfe546c
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2024-04-01 23:42:31 +0100

    Add project files.

---
+++ cliente/cliente.c
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <tchar.h>
+#include <math.h>
+
+#include <stdio.h>
+#include <fcntl.h> 
+#include <io.h>
+
+
+
+int _tmain(int argc, TCHAR* argv[]) {
+
+
+
+
+#ifdef UNICODE
+	_setmode(_fileno(stdin), _O_WTEXT);
+	_setmode(_fileno(stdout), _O_WTEXT);
+	_setmode(_fileno(stderr), _O_WTEXT);
+#endif 
+
+
+
+	return 0;
+}
