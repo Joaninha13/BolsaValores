da72ba746ac5956c383e5a71b0d0dd49e4156dfa
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2024-05-14 21:40:43 +0100

    Server Almost Done

--- bolsa/Utils.h
+++ bolsa/Utils.h
@@ -31,14 +31,16 @@
 typedef struct {
 
 	TCHAR userName[TAM], password[TAM];
-	DWORD saldo;
+	float saldo;
 	BOOL ativo; //TRUE se o usuário está ativo, FALSE caso contrário
+	HANDLE hPipe; // Pipe do cliente
 
 } User;
 typedef struct {
 
 	TCHAR name[TAM];
-	DWORD numAcoes[MAX_USERS], valor; // numAcoes[i] é o número de ações que o usuário i tem. 
+	DWORD numAcoes[MAX_USERS]; // numAcoes[i] é o número de ações que o usuário i tem. 
+	double valor;
 	User usersVenda[MAX_USERS]; // usersVenda[i] é o usuário que está vendendo as ações numAcoes[i]. o i = 0 é o unico null
 
 } CompanyShares;
@@ -46,11 +48,12 @@ typedef struct {
 typedef struct {
 	TCHAR name[TAM];
 	DWORD numAcoes;
-	DWORD valor;
+	double valor;
 } ListCompany;
 
 // Estrutura para a carteira de ações do cliente
 typedef struct {
+	User user;
 	CompanyShares acoes[MAX_EMPRESAS];
 	int numEmpresas; // Número de empresas diferentes na carteira
 } Wallet;
@@ -59,7 +62,7 @@ typedef struct {
 // Estrutura para operações de compra/venda
 typedef struct {
 	BOOL isCompra; // TRUE para compra, FALSE para venda
-	TCHAR nomeEmpresa[TAM];
+	TCHAR nomeEmpresa[TAM], msg[TAM];
 	DWORD quantidadeAcoes;
 } Operation;
 
@@ -67,11 +70,13 @@ typedef struct {
 typedef struct {
 
 	//User user; //Usuario a quem pertence a mensagem
+
 	Operation operacao; // Operação atual (compra/venda)
 	ListCompany listCompany[MAX_EMPRESAS];
 	TCHAR mensagem[TAM]; // Mensagem de resposta da bolsa
 	BOOL sucesso; // TRUE se a operação foi bem sucedida, FALSE caso contrário
-	HANDLE hPipe; // Handle do pipe para responder ao cliente
+
+	//HANDLE hPipe; // Handle do pipe para responder ao cliente
 
 } Response;
 
@@ -91,6 +96,7 @@ typedef struct {
 	HANDLE	hPipes[NCLIENTES],	// Handles dos pipes dos clientes
 			hMutex, // Mutex para proteger a memória compartilhada
 			trinco, // Trinco para proteger o pipeData
+			hMutexData, // Mutex para proteger todos os dados da bolsa
 			hEvent,	// Evento para sinalizar que a memória compartilhada foi atualizada
 			hMap,	// Mapeamento da memória compartilhada
 			hSem;	// Semáforo para controlar o numero de clientes para a criação de pipes
@@ -111,7 +117,8 @@ typedef struct {
 
 	BolsaThreads* bolsaData; // Ponteiro para a estrutura BolsaThreads é preciso para aceder aos hPipes[Nclientes]
 
-	HANDLE hMutex, hPipe;
+	HANDLE hPipe;
+	BOOL continua;
 	DWORD id;
 
 } PipeData;
